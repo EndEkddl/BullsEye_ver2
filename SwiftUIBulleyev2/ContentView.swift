9c14db987a91e1db88217c07f0acd62b86d60bd7
In-Hye Kye <sadnody@gmail.com>
2021-05-03 14:27:08 +0900

    Initial Commit

---
+++ SwiftUIBulleyev2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBulleyev2
+//
+//  Created by RelMac User Exercise2 on 2021/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBulleyev2/SwiftUIBulleyev2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBulleyev2App.swift
+//  SwiftUIBulleyev2
+//
+//  Created by RelMac User Exercise2 on 2021/05/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBulleyev2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
